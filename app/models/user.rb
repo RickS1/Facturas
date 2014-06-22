d3ab959ab482f16dc14b322b2ed89d51c094e68e
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2014-06-21 19:32:16 -0500

    Formulario v1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, 
 :remember_me, :nombre, :rfc, :calle, :numexterno, :numinterno,
 :colonia, :codpostal, :delegacion, :ciudad, :estado,  :tags_attributes
+  validates_presence_of :nombre
+  validates_presence_of :rfc
+  validates_presence_of :calle
+  validates_presence_of :numexterno
+  validates_presence_of :colonia
+  validates_presence_of :codpostal
+  validates_presence_of :delegacion
+  validates_presence_of :ciudad
+  validates_presence_of :estado
+  validates_uniqueness_of :rfc
 end
