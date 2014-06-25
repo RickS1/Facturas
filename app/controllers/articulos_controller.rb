acf529f6e37383844f03a0e13362d57a4e63d179
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2014-06-25 01:53:55 -0500

    Esueleto v1.1

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -1,6 +1,7 @@
 class ArticulosController < ApplicationController
   # GET /articulos
   # GET /articulos.json
+  before_filter :authenticate_user!
   def index
     @articulos = Articulo.all
 
--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,6 +1,7 @@
 class ClientesController < ApplicationController
   # GET /clientes
   # GET /clientes.json
+  before_filter :authenticate_user!
   def index
     @clientes = Cliente.all
 
