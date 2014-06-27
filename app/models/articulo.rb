9b6d9be4071fa904936ecc7710d6944bf33fc31a
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2014-06-27 03:21:53 -0500

    Heroku

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -3,7 +3,7 @@ class Articulo < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :facturas
 
-  attr_accessible :nombre
+  attr_accessible :nombre, :descripcion
 
   def to_label
     "#{nombre}"
--- app/models/articulo.rb~
+++ app/models/articulo.rb~
@@ -2,4 +2,10 @@ class Articulo < ActiveRecord::Base
 
   belongs_to :user
   has_and_belongs_to_many :facturas
+
+  attr_accessible :nombre
+
+  def to_label
+    "#{nombre}"
+  end
 end
