e81c4b45e378aef67dbe93beaaf5d8d015ec4bb1
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2014-07-05 23:45:50 -0500

    Heroku

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -5,6 +5,10 @@ class Articulo < ActiveRecord::Base
 
   attr_accessible :nombre, :descripcion,:precio_u
 
+  validates_presence_of :nombre, :message => "no puede estar vacío."
+  validates_presence_of :precio_u, :message => "no puede estar vacío."
+  validates_numericality_of :precio_u, :message => "debe ser numérico con máximo 2 cifras decimales, mayor que 0.", :greater_than => 0
+
   def to_label
     "#{nombre}"
   end
